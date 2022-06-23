f6cb2982de08535b290d07fe605f9cd12412f94d
Samuel <m.samuelrodrigues.98@gmail.com>
2022-06-23 09:59:31 -0300

    Adiciona funções que realiza operações matematicas basicas
    Adição, subtração, multiplicação e divisão.

---
+++ script.js
@@ -0,0 +1,17 @@
+// MATH OPERATORS FUNCTIONS
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
