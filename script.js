527cacfd89fb00b708e473e291c3e505f5c8c321
Samuel <m.samuelrodrigues.98@gmail.com>
2022-06-23 10:12:23 -0300

    Adiciona função operate
    Função que recebe um operador e dois operandos, e retorna o resultado

--- script.js
+++ script.js
@@ -15,3 +15,19 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+// OPERATE FUNCTION
+
+function operate(operator, operand1, operand2) {
+    if(operator === '+') {
+        return add(operand1, operand2);
+    } else if(operator === '-') {
+        return subtract(operand1, operand2);
+    } else if(operator === '*') {
+        return multiply(operand1, operand2)
+    } else if(operator === '/') {
+        return divide(operand1, operand2);
+    } else {
+        return console.error('ERROR!');
+    }
+}
